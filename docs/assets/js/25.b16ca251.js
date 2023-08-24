acee6bb5df4c004e6b1d6e9c9d0c093728e76a2b
gaodongyu <1393751465@qq.com>
2023-08-24 18:47:21 +0800

    8.25-写在前面的话

--- docs/assets/js/25.b16ca251.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[25],{318:function(t,s,a){"use strict";a.r(s);var n=a(7),e=Object(n.a)({},(function(){var t=this._self._c;return t("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[t("p",[this._v("freezeFish.github.io\ndfsafafdafdafpafa pjasdofdioas")])])}),[],!1,null,null,null);s.default=e.exports}}]);
--- post/.vuepress/config.js
+++ post/.vuepress/config.js
@@ -1,22 +1,22 @@
 module.exports = {
     base: "/freezeFish/",
     lang: "zh-CN",
-    title: "前端",
-    description: "前端",
+    title: "小可爱的找工作日记",
+    description: "小可爱的找工作日记",
     theme: "@qcyblm/vpx",
     themeConfig: {
         search: false,
         nav: [
-            { text: "Home", link: "./", icon: "fa fa-home" },
+            { text: "Home", link: "/", icon: "fa fa-home" },
         ],
         sidebar: [
             {
-                title: "test",
+                title: "八股",
                 collapsable: false, // 不折叠
                 children: [
                     {
-                        title: "test",
-                        path: "./test",
+                        title: "八股1",
+                        path: "/八股",
                     },
                 ],
             },
